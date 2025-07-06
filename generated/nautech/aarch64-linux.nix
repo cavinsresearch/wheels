c5f909062344b33d2f4836dc2b2e0732f4c723e8
jmmaloney4 <5781547+jmmaloney4@users.noreply.github.com>
2025-07-06 06:05:57 +0000

    chore: update wheel definitions

--- generated/nautech/aarch64-linux.nix
+++ generated/nautech/aarch64-linux.nix
@@ -10,11 +10,11 @@
 }: {
   "nautilus_trader-python311" = python311Packages.buildPythonPackage rec {
     pname = "nautilus_trader";
-    version = "1.218.0";
+    version = "1.219.0";
 
     src = fetchurl {
-      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.218.0-cp311-cp311-manylinux_2_35_aarch64.whl";
-      sha256 = "sha256-en2S+MpzJtcssE+8YZRVwVABxXCXg2wTiXxzN+bpP6o=";
+      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.219.0-cp311-cp311-manylinux_2_35_aarch64.whl";
+      sha256 = "sha256-6a6gffFmrQxn2eR8jadwSQ2qUOsH85/ML3XQabtYRnU=";
     };
 
     format = "wheel";
@@ -22,11 +22,11 @@
 
   "nautilus_trader-python312" = python312Packages.buildPythonPackage rec {
     pname = "nautilus_trader";
-    version = "1.218.0";
+    version = "1.219.0";
 
     src = fetchurl {
-      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.218.0-cp312-cp312-manylinux_2_35_aarch64.whl";
-      sha256 = "sha256-GHKQiMXsQHDnsVLqAXcme5wo9WeOyrPGdp61PMR6AYA=";
+      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.219.0-cp312-cp312-manylinux_2_35_aarch64.whl";
+      sha256 = "sha256-uW0gIDjX/Oe/YRzENTgNQ4T8+oFPvBgDsyTEpbUC48k=";
     };
 
     format = "wheel";
@@ -34,11 +34,11 @@
 
   "nautilus_trader-python313" = python313Packages.buildPythonPackage rec {
     pname = "nautilus_trader";
-    version = "1.218.0";
+    version = "1.219.0";
 
     src = fetchurl {
-      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.218.0-cp313-cp313-manylinux_2_35_aarch64.whl";
-      sha256 = "sha256-vQgKgTnHxNuvKjrSVHig47k5zfZWp6ZcBRxmu0yPQeg=";
+      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.219.0-cp313-cp313-manylinux_2_35_aarch64.whl";
+      sha256 = "sha256-sHoCuYqSetplooA0wppAMnbfocMBMiyRiQI4BTjsMEQ=";
     };
 
     format = "wheel";
--- generated/nautech/x86_64-linux.nix
+++ generated/nautech/x86_64-linux.nix
@@ -10,11 +10,11 @@
 }: {
   "nautilus_trader-python311" = python311Packages.buildPythonPackage rec {
     pname = "nautilus_trader";
-    version = "1.218.0";
+    version = "1.219.0";
 
     src = fetchurl {
-      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.218.0-cp311-cp311-manylinux_2_35_x86_64.whl";
-      sha256 = "sha256-cDUB2caTm9rJwd0VacgrRmocnfGRuJgpl6CiG3YmDvE=";
+      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.219.0-cp311-cp311-manylinux_2_35_x86_64.whl";
+      sha256 = "sha256-P1kcX9GEKgy5wPXTaX0faOFQd1Hp0XU+rSWVdoUoVhE=";
     };
 
     format = "wheel";
@@ -22,11 +22,11 @@
 
   "nautilus_trader-python312" = python312Packages.buildPythonPackage rec {
     pname = "nautilus_trader";
-    version = "1.218.0";
+    version = "1.219.0";
 
     src = fetchurl {
-      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.218.0-cp312-cp312-manylinux_2_35_x86_64.whl";
-      sha256 = "sha256-Ua2GdvCiITOMgui+J96N01ln0GZlBc5SWKvaAJJDmLY=";
+      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.219.0-cp312-cp312-manylinux_2_35_x86_64.whl";
+      sha256 = "sha256-+Oy0F8fI+FCsvWEHos3WXFAwl8YqiMMzPfJVZPG10Lo=";
     };
 
     format = "wheel";
@@ -34,11 +34,11 @@
 
   "nautilus_trader-python313" = python313Packages.buildPythonPackage rec {
     pname = "nautilus_trader";
-    version = "1.218.0";
+    version = "1.219.0";
 
     src = fetchurl {
-      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.218.0-cp313-cp313-manylinux_2_35_x86_64.whl";
-      sha256 = "sha256-E2iwuuAq/FdDKT2As83mK3rbTLsdoKlFFch3+XSl8iM=";
+      url = "https://packages.nautechsystems.io/simple/nautilus-trader/nautilus_trader-1.219.0-cp313-cp313-manylinux_2_35_x86_64.whl";
+      sha256 = "sha256-P35rFm8E+6V2bHgqJmj867U6Q6gl5bIqh7iUK2PTbaw=";
     };
 
     format = "wheel";
